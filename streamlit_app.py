a1f1b7d8e9f1ab73c5f4946be0c8922f5f075232
Nattharut <96281587+Nattharut@users.noreply.github.com>
2021-12-17 14:20:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,60 +23,64 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+email = os.environ.get('email')
+password = os.environ.get('password')
+
+"""#### Get access token, token will have 24 hour access time."""
+
+token_url = f'http://54.254.86.214:2002/api/users/login/token/?email=nattharut.nat@cpf.co.th&password=nat221NA'
+response = requests.post(token_url)
+access_token = json.loads(response.content)['access_token']
+
+if access_token not None:
+    with st.echo(code_location='below'):
 
 
-with st.echo(code_location='below'):
+
+
+        total_points = st.slider("Number of points in spiral", 1, 5000, 3000)
+        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+        st.header("Official Date Picker")
+        start_time = st.date_input('start date')
+        
+        end_time = st.date_input('end date')
+        
+        tags = st.text_input('Movie title', 'Life of Brian')
+        st.write('The current movie title is', title)
+
+
+
+
+        
     
- 
-    
-    total_points = st.slider("Number of points in spiral", 1, 5000, 3000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-    
-    
-    email = os.environ.get('email')
-    password = os.environ.get('password')
 
-    """#### Get access token, token will have 24 hour access time."""
+        data_url = f'http://54.254.86.214:2002/api/klaeng/analytics/shrimp-wonton/ti/?start={start_time}&end={end_time}&tags={tags}'
 
-    token_url = f'http://54.254.86.214:2002/api/users/login/token/?email=nattharut.nat@cpf.co.th&password=nat221NA'
-    response = requests.post(token_url)
+        headers_api = {
+            'Authorization': 'Bearer ' + access_token
+        }
 
-    access_token = json.loads(response.content)['access_token']
+        response = requests.get(data_url, headers=headers_api)
+        print(response.status_code)
 
-    """#### Get data from url by selecting start_time, end_time and required tags."""
+        df = pd.DataFrame.from_dict(response.json()['query_results'])
 
-    start_time = '2021-12-01T00:00:00Z'
-    end_time = '2021-12-16T23:59:00Z'
-    tags = '1403,1405,1406'
-
-    data_url = f'http://54.254.86.214:2002/api/klaeng/analytics/shrimp-wonton/ti/?start={start_time}&end={end_time}&tags={tags}'
-
-    headers_api = {
-        'Authorization': 'Bearer ' + access_token
-    }
-
-    response = requests.get(data_url, headers=headers_api)
-    print(response.status_code)
-
-    df = pd.DataFrame.from_dict(response.json()['query_results'])
-
-    pd.set_option('max_colwidth', 400)
-    df = df[['timestamp','tag_index','tag_name','value']]
+        pd.set_option('max_colwidth', 400)
+        df = df[['timestamp','tag_index','tag_name','value']]
 
 
-    df.timestamp = pd.to_datetime(df.timestamp)
-    df.set_index(df.timestamp,inplace=True)
+        df.timestamp = pd.to_datetime(df.timestamp)
+        df.set_index(df.timestamp,inplace=True)
 
-    df.value = round(df.value.astype(int)/10,2)
+        df.value = round(df.value.astype(int)/10,2)
 
-    dfCooker = pd.pivot_table(df, values='value', index=df.index, columns=['tag_index','tag_name'])
-    dfCooker.columns = ['wonton_cooker_temp1','wonton_cooker_temp3','wonton_cooker_temp2']
+        dfCooker = pd.pivot_table(df, values='value', index=df.index, columns=['tag_index','tag_name'])
+        dfCooker.columns = ['wonton_cooker_temp1','wonton_cooker_temp3','wonton_cooker_temp2']
 
-    dfCooker = dfCooker.fillna(method='bfill')
-    
-    st.header("Official Date Picker")
-    st.date_input('start date')
-    st.date_input('end date')
-   
-   
-    st.line_chart(dfCooker)
+        dfCooker = dfCooker.fillna(method='bfill')
+
+        
+
+
+        st.line_chart(dfCooker)
