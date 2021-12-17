ab787b7059c8ec257644a5ffe60ade5c177b67bd
Nattharut <96281587+Nattharut@users.noreply.github.com>
2021-12-17 14:35:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if access_token:
             data_url = f'http://54.254.86.214:2002/api/klaeng/analytics/shrimp-wonton/ti/?start={start_time}&end={end_time}&tags={tags}'
         
             print(data_url)
-
+'''
             headers_api = {
                 'Authorization': 'Bearer ' + access_token
             }
@@ -85,4 +85,4 @@ if access_token:
 
 
             st.line_chart(dfCooker)
-   
+   '''
