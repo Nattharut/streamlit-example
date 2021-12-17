65071ee035a113f4a4fd7b766ccb1d82003c11f0
Nattharut <96281587+Nattharut@users.noreply.github.com>
2021-12-17 14:33:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,44 +46,41 @@ if access_token:
         
         end_time = st.date_input('end date')
         
-        your_name = st.text_input("Enter your name")
-        st.title(your_name)
-
-       
-        #tags = st.text_input("label goes here", '1403,1405,1406')
+        tags = st.text_input("input tags")
+        st.title(tags)
+        if tags and start_time and end_time:
        
 
 
-    '''    
-    
-
-        data_url = f'http://54.254.86.214:2002/api/klaeng/analytics/shrimp-wonton/ti/?start={start_time}&end={end_time}&tags={tags}'
-
-        headers_api = {
-            'Authorization': 'Bearer ' + access_token
-        }
-
-        response = requests.get(data_url, headers=headers_api)
-        print(response.status_code)
-
-        df = pd.DataFrame.from_dict(response.json()['query_results'])
-
-        pd.set_option('max_colwidth', 400)
-        df = df[['timestamp','tag_index','tag_name','value']]
 
 
-        df.timestamp = pd.to_datetime(df.timestamp)
-        df.set_index(df.timestamp,inplace=True)
+            data_url = f'http://54.254.86.214:2002/api/klaeng/analytics/shrimp-wonton/ti/?start={start_time}&end={end_time}&tags={tags}'
 
-        df.value = round(df.value.astype(int)/10,2)
+            headers_api = {
+                'Authorization': 'Bearer ' + access_token
+            }
 
-        dfCooker = pd.pivot_table(df, values='value', index=df.index, columns=['tag_index','tag_name'])
-        dfCooker.columns = ['wonton_cooker_temp1','wonton_cooker_temp3','wonton_cooker_temp2']
+            response = requests.get(data_url, headers=headers_api)
+            print(response.status_code)
 
-        dfCooker = dfCooker.fillna(method='bfill')
+            df = pd.DataFrame.from_dict(response.json()['query_results'])
 
-        
+            pd.set_option('max_colwidth', 400)
+            df = df[['timestamp','tag_index','tag_name','value']]
 
 
-        st.line_chart(dfCooker)
-'''
+            df.timestamp = pd.to_datetime(df.timestamp)
+            df.set_index(df.timestamp,inplace=True)
+
+            df.value = round(df.value.astype(int)/10,2)
+
+            dfCooker = pd.pivot_table(df, values='value', index=df.index, columns=['tag_index','tag_name'])
+            dfCooker.columns = ['wonton_cooker_temp1','wonton_cooker_temp3','wonton_cooker_temp2']
+
+            dfCooker = dfCooker.fillna(method='bfill')
+
+
+
+
+            st.line_chart(dfCooker)
+   
