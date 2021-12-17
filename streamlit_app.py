e43175d346a093341a5ddceae34697a423da09ff
Nattharut <96281587+Nattharut@users.noreply.github.com>
2021-12-17 14:05:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 with st.echo(code_location='below'):
     
-    st.header("Official Date Picker")
-    st.date_input('start date')
-    st.date_input('end date')
+ 
     
     total_points = st.slider("Number of points in spiral", 1, 5000, 3000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
